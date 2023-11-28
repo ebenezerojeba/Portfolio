a9333c4a35bfaa02e39bf66b27a733880220f022
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2023-11-28 11:59:57 +0100

    updated

---
+++ public/script.js
@@ -0,0 +1,40 @@
+
+//Scroll Section
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+     let top = window.scrollY;
+     let offset = sec.offsetTop - 100;
+     let height = sec.offsetHeight;  
+     let id = sec.getAttribute('id');
+
+     if(top >= offset && top < offset + height) {
+        navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+        })
+     }
+    });
+
+    //Sticky Header
+    let header = document.querySelector('header');
+    let sticky = navbar.offsetTop;
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+//Toggle from Navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.getElementById('navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+    
+}
+
